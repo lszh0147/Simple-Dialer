20208985b33ce96cdc514ce020b8ae1019c00f3f
Lszh0147 <no@email.com>
2020-12-18 02:07:16 +0800

    通话记录不合并

--- app/src/main/kotlin/com/simplemobiletools/dialer/helpers/Config.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/helpers/Config.kt
@@ -37,6 +37,6 @@ class Config(context: Context) : BaseConfig(context) {
     fun getCustomSIM(number: String) = prefs.getString(REMEMBER_SIM_PREFIX + number, "")
 
     var groupSubsequentCalls: Boolean
-        get() = prefs.getBoolean(GROUP_SUBSEQUENT_CALLS, true)
+        get() = prefs.getBoolean(GROUP_SUBSEQUENT_CALLS, false)
         set(groupSubsequentCalls) = prefs.edit().putBoolean(GROUP_SUBSEQUENT_CALLS, groupSubsequentCalls).apply()
 }
